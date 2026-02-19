cb269dcf4781079a1392f6400fe965d1692a6584
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2026-02-19 14:14:51 +0300

    changed to arrow function

--- source/partition.js
+++ source/partition.js
@@ -4,21 +4,22 @@
  * Функция, разделяющая массив на два подмассива по условию
  * @param {Array} array - массив значений
  * @param {Function} predicate - условие разделения
- * 
+ *
  * @example
  * // returns [[1, 2], [3]]
  * partition([1, 2, 3], num => num < 3);
- * 
+ *
  * @returns {[Array, Array]}
  */
-function partition(array, predicate) {
+
+const partition = (array, predicate) => {
     if (!Array.isArray(array)) {
         throw new TypeError('array must be an array');
     }
     if (typeof predicate !== 'function') {
         throw new TypeError('predicate must be a function');
     }
-    
+
     return array.reduce((acc, item) => {
         acc[predicate(item) ? 0 : 1].push(item);
         return acc;
